048a38012654c047b2987471ea4825b948712d56
tjwmagic <travis@dragonhelm.net>
2018-02-16 17:25:32 -0600

    Test Upload
    Uploaded a file as a test.

---
+++ generatorName.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#ifndef GENERATORNAME_H
+#define GENERATORNAME_H
+
+class GeneratorName {
+public:
+	//Default Constructor
+	GeneratorName();
+
+	//Deconstructor
+	~GeneratorName();
+
+private:
+
+
+};
+
+#endif // !GENERATORNAME_H
